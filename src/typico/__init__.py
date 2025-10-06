6a1de2a78a324a20207701a8161234cb8babb0d6
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:47:45 +0200

    chore: bump template

--- src/typico/__init__.py
+++ src/typico/__init__.py
@@ -1,8 +1,17 @@
+"""Typico: Type helpers and abstractions"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("typico")
+__title__ = "Typico"
+__description__ = "Type helpers and abstractions"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/typico"
 
 from typico.pyfield import (
     Constraints,
